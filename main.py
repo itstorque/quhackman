9b17269f2a3bb184d4df19b7d6885caced1d1449
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-02-01 13:07:17 -0500

    Added two player support + custom shape based on gifs

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ from random import choice
 from turtle import *
 from freegames import floor, vector
 
-past_input = None
+past_input_a = None
+past_input_b = None
+HARDCODE_BOTH = False
+# shape("turtle")
+register_shape('player.gif')
+shape('player.gif')
+resizemode('auto')
 
 state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
+aim2 = vector(5, 0)
 pacman = vector(-40, -80)
+pacman2 = vector(-20, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
@@ -105,7 +113,8 @@ def world():
 def move():
     "Move pacman and all ghosts."
 
-    global past_input
+    global past_input_a
+    global past_input_b
 
     writer.undo()
     writer.write(state['score'])
@@ -115,9 +124,17 @@ def move():
     if valid(pacman + aim):
         pacman.move(aim)
 
-    if past_input != None:
-        change(*past_input, save=False)
+    if valid(pacman2 + aim2):
+        pacman2.move(aim2)
+
+    if past_input_a != None:
+        change(*past_input_a, "a", save=False)
+
+    if past_input_b != None:
+        change(*past_input_b, "b", save=False)
+
     index = offset(pacman)
+    index2 = offset(pacman2)
 
     if tiles[index] == 1:
         tiles[index] = 2
@@ -128,7 +145,19 @@ def move():
 
     up()
     goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
+    resizemode('auto')
+    penup()
+    turtlesize(0.1)
+    color('green')
+    stamp()
+
+    up()
+    goto(pacman2.x + 10, pacman2.y + 10)
+    resizemode('auto')
+    penup()
+    turtlesize(0.1)
+    color('green')
+    stamp()
 
     for point, course in ghosts:
         if valid(point + course):
@@ -153,19 +182,30 @@ def move():
     for point, course in ghosts:
         if abs(pacman - point) < 20:
             return
+        if abs(pacman2 - point) < 20:
+            return
 
     ontimer(move, 100)
 
-def change(x, y, save=True):
+def change(x, y, both=True, save=True):
     "Change pacman aim if valid."
 
-    global past_input
+    global past_input_a
+    global past_input_b
 
-    if save: past_input = (x, y)
-    if valid(pacman + vector(x, y)):
+    a, b = both==True or both=="a" or HARDCODE_BOTH, both==True or both=="b" or HARDCODE_BOTH
+
+    if save and a: past_input_a = (x, y)
+    if save and b: past_input_b = (x, y)
+
+    if valid(pacman + vector(x, y)) and a:
         aim.x = x
         aim.y = y
-        past_input = None
+        past_input_a = None
+    if valid(pacman2 + vector(x, y)) and b:
+        aim2.x = x
+        aim2.y = y
+        past_input_b = None
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -174,11 +214,14 @@ writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
 listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
+onkey(lambda: change(5, 0, "a"), 'Right')
+onkey(lambda: change(-5, 0, "a"), 'Left')
+onkey(lambda: change(0, 5, "a"), 'Up')
+onkey(lambda: change(0, -5, "a"), 'Down')
+onkey(lambda: change(5, 0, "b"), 'd')
+onkey(lambda: change(-5, 0, "b"), 'a')
+onkey(lambda: change(0, 5, "b"), 'w')
+onkey(lambda: change(0, -5, "b"), 's')
 world()
 move()
 done()
-
